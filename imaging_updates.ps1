a669d94bb812ff69228abbd5570ce447688b030e
mccpete <125607097+mccpete@users.noreply.github.com>
2023-08-08 01:31:08 +0000

    Add files via upload

---
+++ imaging_updates.ps1
@@ -0,0 +1,47 @@
+﻿Write-Host "Starting Post imaging Updates and checks......."
+
+start-sleep -s 3
+
+#Get Current Setting before change
+Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" | select ShellFeedsTaskbarViewMode
+ 
+#Remove News and Interest Using Powershell
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2
+ 
+#Get Current Setting after change
+Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" | select ShellFeedsTaskbarViewMode
+
+#Get rid of search bar
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name 'ShowTaskViewButton' -Type 'DWord' -Value 0
+
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name 'SearchboxTaskbarMode' -Type 'DWord' -Value 1
+
+#refesh taskbar process
+taskkill /f /im explorer.exe
+
+start-sleep -s 3
+
+#restart taskbar process
+start explorer.exe
+
+write-host "Removed News and Interests"
+
+start-sleep -s 3
+
+start ms-settings:windowsupdate
+
+write-host "Launched Windows Update"
+
+cd C:\
+
+cd "Program Files"
+
+cd .\Dell\CommandUpdate
+
+start-sleep -s 3
+
+.\dcu-cli.exe /ApplyUpdates -reboot=disable
+
+write-host "Launched Dell Command Update"
+
+write-host "Post imaging script COMPLETED!!"
